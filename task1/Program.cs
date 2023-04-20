bd7a386ef394bfcfde743e6f05c6274405fcdf4b
Zeradeil <wandark@ya.ru>
2023-04-20 20:49:43 +0700

    решено 2

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,3 +1,41 @@
 ﻿// Задача 1: Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
 
 
+int m = Readnum("Введите количество строк массива => ");
+int n = Readnum("Введите количество столбцов массива => ");
+System.Console.WriteLine($"{m}, {n}");
+float[,] massive = FillArray(m, n);
+Print(massive);
+
+int Readnum(string massage)
+{
+    System.Console.Write(massage);
+    return Convert.ToInt32(System.Console.ReadLine());
+}
+
+float[,] FillArray(int m, int n)
+{
+    Random rand = new Random();
+    float[,] array = new float[m, n];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rand.NextDouble();
+        }
+    }
+    return array;
+
+}
+
+void Print(float[,] FillArray)
+{
+    for (int i = 0; i < massive.GetLength(0); i++)
+    {
+        for (int j = 0; j < massive.GetLength(1); j++)
+        {
+            System.Console.Write($"{massive[i, j]} \t");
+        }
+        System.Console.WriteLine();
+    }
+}
--- task2/Program.cs
+++ task2/Program.cs
@@ -1,5 +1,52 @@
 ﻿// Задача 2: Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
 //  и возвращает значение этого элемента или же указание, что такого элемента нет.
 
+int m = Readnum("Введите количество строк массива => ");
+int n = Readnum("Введите количество столбцов массива => ");
+int[,] massive = FillArray(m, n);
+Print(massive);
+int row = Readnum("Введите строку массива => ");
+int colum = Readnum("Введите столбец массива => ");
+if (row <= m && colum <= n)
+{
+    System.Console.WriteLine($"{massive[row - 1, colum - 1]}");
+}
+else
+{
+    System.Console.WriteLine("Этого элемента нет");
+}
+
+int Readnum(string massage)
+{
+    System.Console.Write(massage);
+    return Convert.ToInt32(System.Console.ReadLine());
+}
+
+int[,] FillArray(int m, int n)
+{
+    Random rand = new Random();
+    int[,] array = new int[m, n];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rand.Next(-10, 10);
+        }
+    }
+    return array;
+
+}
+
+void Print(int[,] FillArray)
+{
+    for (int i = 0; i < massive.GetLength(0); i++)
+    {
+        for (int j = 0; j < massive.GetLength(1); j++)
+        {
+            System.Console.Write($"{massive[i, j]} \t");
+        }
+        System.Console.WriteLine();
+    }
+}
 
 
